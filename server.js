16b706eb36c442ad2b8a5f53643ded8812bfde11
KangJuHyeon <alinos@naver.com>
2022-04-25 02:02:46 +0900

    sequelize을 이용해서 DB 연결 (#1)

--- server.js
+++ server.js
@@ -1,54 +1,30 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const logger = require('morgan');
+const db = require("./models");
+const logger = require("morgan");
 const port = 3000;
-const apiRouter = express.Router();
 
-app.use(express.json())
-app.use(logger('dev', {}));
-app.use(express.urlencoded({
-  extended: true
-}));
+// const indexRouter = require("./main/index");
 
-app.use('/api', apiRouter);
+db.sequelize
+    .sync()
+    .then(() => {
+        console.log("db 연결 성공 ");
+    })
+    .catch(console.error);
 
-apiRouter.post('/sayHello', (req, res) => {
-    const responseBody = {
-      version: "2.0",
-      template: {
-        outputs: [
-          {
-            simpleText: {
-              text: "hello I'm Ryan"
-            }
-          }
-        ]
-      }
-    };
-  
-    res.status(200).send(responseBody);
-  });
-  
-apiRouter.post('/showHello', (req, res) => {
-    console.log(req.body);
-  
-    const responseBody = {
-      version: "2.0",
-      template: {
-        outputs: [
-          {
-            simpleImage: {
-              imageUrl: "https://t1.daumcdn.net/friends/prod/category/M001_friends_ryan2.jpg",
-              altText: "hello I'm Ryan"
-            }
-          }
-        ]
-      }
-    };
-  
-    res.status(200).send(responseBody);
-  });
+app.use(express.json());
+app.use(logger("dev", {}));
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+);
+
+// Main Route
+app.use(require("./main"));
+// app.use("/", indexRouter);
 
 app.listen(port, () => {
-  console.log(`Example skill server listening on ${port}!`);
+    console.log(`Example skill server listening on ${port}!`);
 });
